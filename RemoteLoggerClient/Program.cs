ce2aef4e30198066515cf0ee29f955a278272836
Stefano Di Nucci <stefanodinucci@gmail.com>
2023-06-24 22:04:02 +0200

    Retrieve metrics

--- RemoteLoggerClient/Program.cs
+++ RemoteLoggerClient/Program.cs
@@ -1,64 +1,102 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
-static async Task Main()
+
+while (true)
 {
-    Console.WriteLine(GetMemoryStats().MemoryAvailable);
+    Console.WriteLine(GetRamUsage());
+    Console.WriteLine(GetDiskSpace());
+    Console.WriteLine(GetCpuName());
+    Console.WriteLine("Cpu: " + GetCpuUsage());
+    GetNetworkInfo();
+    await Task.Delay(3000);
 }
 
-static MemoryStatsDto GetMemoryStats()
+static long GetRamUsage()
 {
-    MemoryStatsDto memoryStatsDto = new MemoryStatsDto();
-    string memoryStatsPath = "//proc//meminfo";
-    if (File.Exists(memoryStatsPath))
     {
-        var file = File.ReadAllLines(memoryStatsPath);
+        var proc = Process.GetProcesses();
+        return proc.Sum(x => x.PrivateMemorySize64) / 1024 / 1024;
+    }
+}
 
-        //Skipping all lines we are not interested in
-        for (int i = 0; i < 3; i++)
+static int GetDiskSpace()
+{
+    var drive = new DriveInfo("."); //https://stackoverflow.com/a/62973033
+
+    var totalGigaBytes = drive.TotalSize / 1024 / 1024 / 1024;
+    var freeGigaBytes = drive.AvailableFreeSpace / 1024 / 1024 / 1024;
+
+    var freePercent = (int)((100 * freeGigaBytes) / totalGigaBytes);
+
+    return freePercent;
+}
+
+static string GetCpuName()
+{
+    string cpuName = string.Empty;
+    using var reader = new StreamReader("/proc/cpuinfo");
+    string line;
+
+    while ((line = reader.ReadLine()) != null)
+    {
+        if (line.StartsWith("model name"))
         {
-            int firstOccurenceOfDigit = 0;
-            var memoryLine = file[i];
-            //index of first number , start the string until the end and store it
-            for (int j = 0; j < memoryLine.Length; j++)
-            {
-                if (Char.IsNumber(memoryLine[j]))
-                {
-                    firstOccurenceOfDigit = j;
-                    break;
-                }
-            }
+            int startIndex = line.IndexOf(':') + 2;
+            cpuName = line[startIndex..];
+            break;
+        }
+    }
 
-            var memoryValue = memoryLine.Substring(firstOccurenceOfDigit);
+    return cpuName;
+}
 
-            switch (i)
+// https://www.baeldung.com/linux/get-cpu-usage#2-getting-cpu-usage-using-procstat
+static long GetCpuUsage()
+{
+    using var reader = new StreamReader("/proc/stat");
+    string line = reader.ReadLine();
+
+    if (line != null && line.StartsWith("cpu "))
+    {
+        var cpuStats = line
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Skip(1)
+            .ToArray();
+
+        var idle = long.Parse(cpuStats[3]);
+        var total = cpuStats.Sum(x=>long.Parse(x));
+
+        var averageIdleTime = (idle * 100) / total;
+        return 100 - averageIdleTime;
+    }
+
+    return 0;
+}
+
+static void GetNetworkInfo()
+{
+
+    using (var reader = new StreamReader("/proc/net/dev"))
+    {
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            if (line.Contains(":"))
             {
-                case 0:
-                    memoryStatsDto.MemoryTotal = memoryValue;
-                    break;
-                case 1:
-                    memoryStatsDto.MemoryFree = memoryValue;
-                    break;
-                case 2:
-                    memoryStatsDto.MemoryAvailable = memoryValue;
-                    break;
-                default: break;
+                string[] parts = line.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                string interfaceName = parts[0].Trim();
+                string[] stats = parts[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                long receivedBytes = long.Parse(stats[0]);
+                long transmittedBytes = long.Parse(stats[8]);
+
+                Console.WriteLine($"Interface: {interfaceName}");
+                Console.WriteLine($"Received Bytes: {receivedBytes}");
+                Console.WriteLine($"Transmitted Bytes: {transmittedBytes}");
+                Console.WriteLine();
             }
         }
-
-        return memoryStatsDto;
     }
-    else
-    {
-        memoryStatsDto.MemoryAvailable = "";
-        memoryStatsDto.MemoryFree = "";
-        memoryStatsDto.MemoryTotal = "";
-        return memoryStatsDto;
-    }
-}
-
-public class MemoryStatsDto
-{
-    public string MemoryAvailable { get; set; }
-    public string MemoryFree { get; set; }
-    public string MemoryTotal { get; set; }
 }
