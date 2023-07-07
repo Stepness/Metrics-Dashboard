2a63061241ceea6853e1c93c0a68e69d8008a815
Stepness <stefanodinucci@gmail.com>
2023-07-07 20:04:08 +0200

    Refactor js script location

--- Dashboard/assets/scripts/index.js
+++ Dashboard/assets/scripts/index.js
@@ -23,4 +23,56 @@ async function checkRoleClaim() {
 
   } 
 }
-  
+
+
+let connection = new signalR.HubConnectionBuilder()
+//.withUrl("https://localhost:8080/hubs/metrics", {
+.withUrl("https://metrics-monitoring-server.azurewebsites.net/hubs/metrics", {
+accessTokenFactory: () => {
+  return localStorage.getItem('jwtToken');
+}
+})
+.configureLogging(signalR.LogLevel.Information)
+.build();
+
+const lastReceivedDataTimestamps = new Map();
+
+function healthCheck(){ 
+  setInterval(() => {
+    const currentTimestamp = Date.now();
+    lastReceivedDataTimestamps.forEach((date, connectionId) => {
+      const lastReceivedTimestamp = date;
+      const elapsedTime = currentTimestamp - lastReceivedTimestamp;
+      
+      if (elapsedTime >= 3000) {
+        setUnhealthy(connectionId);
+      }
+    });
+  }, 1000);
+}
+
+async function startSignalR(){
+  async function start() {
+    try {
+      await connection.start();
+      console.log("SignalR Connected.");
+      await connection.invoke("JoinDashboardGroup");
+    } catch (err) {
+        console.log(err);
+    }
+  }
+
+  connection.on("ReceiveBroadcastMessage", (dashboardDto) => {
+      console.log(dashboardDto);
+      if($('#index-error-message'))
+        $('#index-error-message').text("")
+      lastReceivedDataTimestamps.set(dashboardDto.connectionId, Date.now());
+      generateOrUpdateCard(dashboardDto)
+  })
+
+  await connection.onclose(async () => {
+      await start();
+  });
+
+  await start();
+}
