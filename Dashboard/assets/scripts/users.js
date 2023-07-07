10931dfb821c6b459edac8ada006e49dea4a4e59
Stepness <stefanodinucci@gmail.com>
2023-07-08 01:19:33 +0200

    Add users table style

--- Dashboard/assets/scripts/users.js
+++ Dashboard/assets/scripts/users.js
@@ -11,4 +11,42 @@ function usersPageLoad(){
   }
 
   fillUserMenu(role, name);
+  generateUserTable();
+}
+
+function generateUserTable(){
+  // User data (example)
+  var users = [
+    { name: 'User 1', id: 1 },
+    { name: 'User 2', id: 2 },
+    { name: 'User 3', id: 3 }
+  ];
+
+  users.forEach(function(user) {
+    var listItem = $('<li></li>');
+    var userButton = $('<button></button>').text(user.name);
+    userButton.click(function() {
+      // sendHttpRequest(user.name);
+    });
+    listItem.append(userButton);
+    $('#user-list').append(listItem);
+  });
+
+  // Function to send HTTP request with user name
+  function sendHttpRequest(userName) {
+    // Replace this code with your actual HTTP request logic
+    // Example using jQuery AJAX:
+    $.ajax({
+      url: 'http://example.com/api/users',
+      method: 'POST',
+      data: JSON.stringify({ name: userName }),
+      contentType: 'application/json',
+      success: function(response) {
+        // Handle the response
+      },
+      error: function(error) {
+        // Handle the error
+      }
+    });
+  }
 }
