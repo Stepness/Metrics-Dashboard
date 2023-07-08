9e091f03de6c2f8eb21c4a3a9be12756ded0a259
Stepness <stefanodinucci@gmail.com>
2023-07-08 15:37:17 +0200

    Allow any method

--- MetricsMonitoringServer/Program.cs
+++ MetricsMonitoringServer/Program.cs
@@ -50,6 +50,7 @@ builder.Services.AddCors(options =>
             policy
                 .AllowAnyHeader()
                 .AllowCredentials()
+                .AllowAnyMethod()
                 .WithOrigins("http://localhost:5500", "http://127.0.0.1:5500", "https://gentle-wave-0e686da03.3.azurestaticapps.net");
         });
 });
